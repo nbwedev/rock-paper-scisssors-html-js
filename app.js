a7faf9cecbe128e4b1638852113a2ca3259d1878
Neil Benolirao <neilskillsets.initiative@gmail.com>
2025-07-11 19:27:01 +0800

    Added DOM elements in app.js

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-// Returns players input
-const getHumanChoice = () => {
-  let humanChoice = prompt("Choose: rock, paper, scissors");
-  return humanChoice.toLowerCase();
-};
+// Caching DOM elements
+// Appended corresponding HTML tags in DOM elements naming convention for easier debugging/tracing
 
-// Returns computer choice using rounded random numbers
-const getComputerChoice = () => {
-  let randomNumber = Math.floor(Math.random() * 3);
+let userScore = 0;
+let computerScore = 0;
 
-  let computerChoice =
-    randomNumber === 0 ? "rock" : randomNumber === 1 ? "paper" : "scissors";
+const userScore_span = document.getElementById("human-score");
+const computerScore_span = document.getElementById("comp-score");
+const scoreBaord_div = document.querySelector(".scoreboard");
 
-  return computerChoice;
-};
+const result_div = document.querySelector(".result");
 
-// Game logic when playing 1 round
-const playRound = (humanChoice, computerChoice) => {
-  if (humanChoice === computerChoice) {
-    return "Tie";
-  }
-  if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    return `You win! ${humanChoice} beats ${computerChoice}`;
-  } else {
-    return `You lose! ${computerChoice} beats ${humanChoice}`;
-  }
-};
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
